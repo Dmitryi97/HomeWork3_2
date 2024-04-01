5f86c69d145a37574174558c8b0123957322f4a7
Dmitryi <pytin97@bk.ru>
2024-04-01 21:48:13 +0300

    Выполнил ДЗ!

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Transport[] transport = {
+                new Car("car1", 4),
+                new Car("car2", 4),
+                new Truck("truck1", 6),
+                new Truck("truck2", 8),
+                new Bicycle("bicycle1", 2),
+                new Bicycle("bicycle2", 2),
+        };
+        transport[0].check();
+        transport[1].check();
+        transport[2].check();
+        transport[3].check();
+        transport[4].check();
+        transport[5].check();
     }
 }
